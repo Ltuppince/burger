906e84703be62ce8245abd1d940d31594c1f72d3
Lorenzo T. Tuppince <lorenzot.tuppince@Macs-MacBook-Pro.local>
2020-02-11 01:26:45 -0500

    new commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Patty melt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plant Base', true );
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie burger', true);
